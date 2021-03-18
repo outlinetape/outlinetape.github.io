026e7764cab79a957360c520ddcf875f6aff6fef
outlinetape <75751450+outlinetape@users.noreply.github.com>
2021-03-18 17:31:00 +0100

    upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// How can I open the fixed mixcloud player at the footage by clicking the picture links - and it stays 
+// mixcover clicking animation - play button
+// section text-pic organizing in a responsible way
+// cut the main picture to an orb form
+// js animation for fading
+// menu background has opacity, but not the text
+// phone size, responsibility
+// accessability check
+// typo?
+// outline button animation, hogy lehet, hogy a keret is valtozik
+// backarrow buttons
+// language changes
+// outline mixcloud szamozas
+// minden felulet ujrarendezes
+// csinalj mixeket is, lol
+
+// kerdesek Bencenek >>>
+
+// broadcast site autofill miafaszer nem mukodik
+// ugyanitt miert fut a header elott a kep
+// play button animaciot hogz erdemes csinalni
+// style tagok hogy mukodnek, merre keressek
+// fixed position problem at past events
+// tageket hogy csinaljam
+// folyamatoslejatszas problemajanak kerdese
+// social media ikon mizeria
+// outline button kulso resz hover bug
+// ay interjuknal a 100% miert nem az, miert log ki alola a hatterkep es az alja
+
+
+
+// tilos.hu-t is kigereblyezni
+// tilos korai kepeket felgongyoliteni
---
+++ talk/script.js
@@ -0,0 +1,15 @@
+// How can I ope the fixed mixcloud player at the footage by clicking the picture links - and it stays 
+// mixcover clicking animation - play button
+// section text-pic organizing in a responsible way
+// cut the main picture to an orb form
+// js animation for fading
+// menu background has opacity, but not the text
+// phone size, responsibility
+// accessability check
+// typo?
+// outline button animation, hogy lehet, hogy a keret is valtozik
+// backarrow buttons
+// language changes
+// outline mixcloud szamozas
+// minden felulet ujrarendezes
+// csinalj mixeket is, lol
